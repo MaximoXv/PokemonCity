fe8e26bebd03fa5d2fe22c160f7838686279a505
MaximoXv <maximoibarra164@gmail.com>
2026-06-18 17:46:32 -0300

    fix: resourse path for sounds

--- sound_manager.py
+++ sound_manager.py
@@ -1,16 +1,18 @@
 import pygame
 
+from utils import resource_path
+
 
 class SoundManager:
 
     def __init__(self):
 
         self.sounds = {
-            "click": pygame.mixer.Sound("assets/sfx/click.wav"),
-            "collect_food": pygame.mixer.Sound("assets/sfx/collect_food.mp3"),
-            "collect_gold": pygame.mixer.Sound("assets/sfx/collect_gold.mp3"),
-            "buy": pygame.mixer.Sound("assets/sfx/buy.mp3"),
-            "error": pygame.mixer.Sound("assets/sfx/error.mp3"),
+            "click": pygame.mixer.Sound(resource_path("assets/sfx/click.wav")),
+            "collect_food": pygame.mixer.Sound(resource_path("assets/sfx/collect_food.mp3")),
+            "collect_gold": pygame.mixer.Sound(resource_path("assets/sfx/collect_gold.mp3")),
+            "buy": pygame.mixer.Sound(resource_path("assets/sfx/buy.mp3")),
+            "error": pygame.mixer.Sound(resource_path("assets/sfx/error.mp3")),
         }
 
         for sound in self.sounds.values():
